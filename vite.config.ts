3f29c3a0e245a62b5456db79cdb374bb06ecad0a
Costin Gheorghe <cgheorghe0702@stud.acs.upb.ro>
2025-02-16 13:35:07 +0200

    [FIX]

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
       input:{
         main: resolve(__dirname, 'index.html'),
         studio_main: resolve(__dirname, 'studio.html'),
-        web_cam_main: resolve(__dirname, 'webcam.html'),
       },
     },
   },
